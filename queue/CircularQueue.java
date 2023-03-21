7980ea625b1b6202ad03ffc9de6851d15105a3e8
alok-karn <alokkyasth@gmail.com>
2023-03-21 14:59:02 +0530

    ➕ queue

--- queue/CircularQueue.java
+++ queue/CircularQueue.java
@@ -1,3 +1,100 @@
+class CircularData {
+    final int maxSize = 1000;
+    int front, rear;
+    int items[] = new int[maxSize];
+
+    CircularData() {
+        front = -1;
+        rear = -1;
+    }
+
+    boolean isFull() {
+        if (front == 0 && rear == maxSize - 1) {
+            return true;
+        }
+        if (front == rear + 1) {
+            return true;
+        }
+        return false;
+    }
+
+    boolean isEmpty() {
+        if (front == -1)
+            return true;
+        else
+            return false;
+    }
+
+    void insertItem(int key) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            if (front == -1)
+                front = 0;
+            rear = (rear + 1) % maxSize;
+            items[rear] = key;
+            System.out.println("Inserted " + key);
+        }
+    }
+
+
+    int deleteItem() {
+        int key;
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return (-1);
+        } else {
+            key = items[front];
+            if (front == rear) {
+                front = -1;
+                rear = -1;
+            } /* Q has only one key, so we reset the queue after deleting it. */
+            else {
+                front = (front + 1) % maxSize;
+            }
+            return (key);
+        }
+    }
+
+    void displayItems() {
+        int i;
+
+        if (isEmpty()) {
+            System.out.println("Empty Queue");
+        } else {
+            System.out.println("Front -> " + front);
+            System.out.println("Items -> ");
+            for (i = front; i != rear; i = (i + 1) % maxSize)
+                System.out.print(items[i] + " ");
+            System.out.println(items[i]);
+            System.out.println("Rear -> " + rear);
+
+        }
+    }
+
+
+   
+}
 public class CircularQueue {
-    
+    public static void main(String[] args) {
+        CircularData obj = new CircularData();
+
+        obj.insertItem(1);
+        obj.insertItem(2);
+        obj.insertItem(3);
+        obj.insertItem(4);
+        obj.insertItem(5);
+        obj.insertItem(6);
+        obj.displayItems();
+        int key = obj.deleteItem();
+       
+        if(key != -1)
+            System.out.println("Deleted Element is " + key);
+        obj.displayItems();
+
+        obj.insertItem(7);
+        obj.displayItems();
+        obj.insertItem(10);
+
+    }
 }
--- queue/DoubleEndedQueue.java
+++ queue/DoubleEndedQueue.java
@@ -1,3 +1,110 @@
+
+
+class DoubleQueue {
+    private int maxSize = 1000;
+    private long[] queArray;
+    private int front;
+
+    private int rear;
+    private int nItems;
+
+    public DoubleQueue(int s) {
+        maxSize = s;
+        queArray = new long[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
+    }
+
+
+
+    public void insertLeft(long j) {
+        if (rear == maxSize - 1) {
+            rear = -1;
+        }
+        queArray[++rear] = j;
+
+        System.out.println(j + " inserted at rear" + rear);
+        nItems++;
+    }
+
+    public void insertRight(long j) {
+        if (front == 0) {
+            front = maxSize;
+        }
+        queArray[--front] = j;
+        System.out.println(j + " inserted at front" + front);
+
+        nItems++;
+    }
+
+    public long removeLeft() {
+        long temp = queArray[front++];
+        if (front == maxSize) {
+            front = 0;
+        }
+        System.out.println(nItems + " removed at front" + front);
+        nItems--;
+        return temp;
+    }
+
+    public long removeRight() {
+        long temp = queArray[rear--];
+        if (rear == -1) {
+            rear = maxSize - 1;
+        }
+        System.out.println(nItems + " removed at rear" + rear);
+        nItems--;
+        return temp;
+    }
+
+    public long peekFront() {
+        return queArray[front];
+    }
+
+    public boolean isEmpty() {
+        return (nItems == 0);
+    }
+
+    public boolean isFull() {
+        return (nItems == maxSize);
+    }
+
+    public int size() {
+        return nItems;
+    }
+
+}
 public class DoubleEndedQueue {
+    public static void main(String[] args) {
+
+        DoubleQueue theQueue = new DoubleQueue(5);
+
+        theQueue.insertLeft(10);
+        theQueue.insertLeft(20);
+        theQueue.insertLeft(30);
+        theQueue.insertLeft(40);
+        theQueue.insertLeft(50);
+
+        theQueue.insertRight(60);
+        theQueue.insertRight(70);
+        theQueue.insertRight(80);
+        theQueue.insertRight(90);
+        theQueue.insertRight(100);
+
+        while (!theQueue.isEmpty()) {
+            long n = theQueue.removeLeft();
+            System.out.print(n);
+            System.out.print(" ");
+        }
+        System.out.println("");
+        while (!theQueue.isEmpty()) {
+            long n = theQueue.removeRight();
+            System.out.print(n);
+            System.out.print(" ");
+        }
+        
+    }
     
 }
+
